32ec4a8a0e09881bc9fc20da89f9df426d043291
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2021-08-01 15:09:59 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Fleck;
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OnlineUserCounter
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var socketsMap = new Dictionary<string, List<IWebSocketConnection>>();
+            var server = new WebSocketServer("ws://0.0.0.0");
+            //var server = new WebSocketServer("wss://0.0.0.0");
+            //server.Certificate = new X509Certificate2("MyCert.pfx", "password");
+            server.Start(socket =>
+            {
+                var protocol = socket.ConnectionInfo.SubProtocol;
+                socketsMap.TryAdd(protocol, new List<IWebSocketConnection>());
+                var sockets = socketsMap[protocol];
+                socket.OnOpen = () =>
+                {
+                    sockets.Add(socket);
+                    SendMessageToSockets(sockets, sockets.Count.ToString());
+                };
+                socket.OnClose = () =>
+                {
+                    sockets.Remove(socket);
+                    SendMessageToSockets(sockets, sockets.Count.ToString());
+
+                };
+            });
+            Console.ReadKey();
+        }
+        static void SendMessageToSockets(IEnumerable<IWebSocketConnection> sockets, string message)
+        {
+            foreach (var socket in sockets)
+            {
+                socket.Send(message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# OnlineUserCounter
+A simple C# (.NET 5) websocket server to count online users using [Fleck](https://github.com/statianzo/Fleck).
